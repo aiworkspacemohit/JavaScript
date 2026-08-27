bd80be416cc07f9c6dfaa9a6eb3dab99faeecc33
Mohit <ai.workspace.mohit@gmail.com>
2026-08-27 06:25:26 +0000

    conversion of operations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,44 @@
+let score = "33"
+
+console.log(typeof score);
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let value = "45aa"
+console.log(typeof(value));
+let valueInValue = Number(value);
+console.log(typeof(valueInValue));
+console.log(valueInValue);
+
+
+// "33" => 33
+// "33abs" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1 
+console.log(typeof(isLoggedIn))
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(typeof(booleanIsLoggedIn))
+console.log(booleanIsLoggedIn);
+
+// "" => false
+// "mohit singh" => true 
+
+//**********0perations********* */
+
+let opValue = 3
+let negValue = -opValue
+console.log(negValue);
+
+/* 
+console.log(2+2);
+console.log(2*3);
+console.log(2**3);
+*/
+
+console.log("1"+2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "3");
