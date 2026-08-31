a19c5c1bed94fe8caaf3176e67daae4edcc10ca1
Mohit <ai.workspace.mohit@gmail.com>
2026-08-31 07:22:23 +0000

    dom

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -1,44 +1,50 @@
 const user = {
     username : "hitesh",
-    price: 333,
+    username: "hardeep",
+    
+    // price: 333,
 
     welcomeMessage : function(){
-        console.log(`${this.username} , welcome to website`)
+        console.log(`${this.user} , welcome to website`)
         console.log(this);
     }
 }
+
+// console.log(this.username);
+// console.log(this.user.welcomeMessage);
+
 
 // user.welcomeMessage()
 // user.username = "mohit"
 // user.welcomeMessage()
 
-// console.log(this);
+console.log(this);
 
-function chai(){
-    let username = "hitesh"
-    console.log(this);
-}
-
-// chai()
-
-// const chaiwala = function() {
+// function chai(){
 //     let username = "hitesh"
 //     console.log(this);
 // }
 
-const chaiwala = () => {
-    let username = "hitesh"
-    console.log(this);
-}
- 
-chaiwala()
+// // chai()
 
-// const addTwo = (num1, num2) => {
-//     return num1 + num2
+// // const chaiwala = function() {
+// //     let username = "hitesh"
+// //     console.log(this);
+// // }
+
+// const chaiwala = () => {
+//     let username = "hitesh"
+//     console.log(this);
 // }
+ 
+// chaiwala()
 
-const addTwo = (num1, num2) => (num1 + num2)
-// when we use curly braces return must be written between them 
-// but in parathesis there is no need
-// object must be in the curly braces 
-console.log(addTwo(3,4));
+// // const addTwo = (num1, num2) => {
+// //     return num1 + num2
+// // }
+
+// const addTwo = (num1, num2) => (num1 + num2)
+// // when we use curly braces return must be written between them 
+// // but in parathesis there is no need
+// // object must be in the curly braces 
+// console.log(addTwo(3,4));
