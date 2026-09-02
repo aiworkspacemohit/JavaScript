fbcf044f580b0bc98b7578c149255003ef5bf8b0
Mohit <ai.workspace.mohit@gmail.com>
2026-09-02 05:38:35 +0000

    catch in promise

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -1,46 +1,54 @@
 // fetch('https://something.comm').then().catch().finally()
 
-const promiseOne = new Promise(function(resolve, reject){
-    //Do an async task
-    //db calls, cryptography, network
-    setTimeout(function(){
-        console.log('Async task is completed');
-        resolve();
-    },1000);
-});
+// const promiseOne = new Promise(function(resolve, reject){
+//     //Do an async task
+//     //db calls, cryptography, network
+//     setTimeout(function(){
+//         console.log('Async task is completed');
+//         resolve();
+//     },1000);
+// });
 
-promiseOne.then(function(){
-    console.log("Promise consumed");
-});
+// promiseOne.then(function(){
+//     console.log("Promise consumed");
+// });
 
-new Promise(function(resolve, reject){
-    setTimeout(function(){
-        console.log("Async task Two:");
-        resolve();
-    },1000);
-}).then(function(){
-    console.log("Async 2 is resolved");
-});
+// new Promise(function(resolve, reject){
+//     setTimeout(function(){
+//         console.log("Async task Two:");
+//         resolve();
+//     },1000);
+// }).then(function(){
+//     console.log("Async 2 is resolved");
+// });
 
 
 
-const promiseThree = new Promise(function(resolve, reject){
-    setTimeout(function(){
-        resolve({username: "chai", email: "chai@example.com"});
-    },1000)
-})
+// const promiseThree = new Promise(function(resolve, reject){
+//     setTimeout(function(){
+//         resolve({username: "chai", email: "chai@example.com"});
+//     },1000)
+// })
 
-promiseThree.then(function(user){
-    console.log(user);
-});
+// promiseThree.then(function(user){
+//     console.log(user);
+// });
 
 const promiseFour = new Promise(function(resolve, reject){
     setTimeout(function(){
         let error = true;
         if(!error){
             resolve({username:"mohit", email:"mohit@example.com"});
+        } else {
+            reject('Error: Something went wrong');
         }
-    });
-}).then(function(user){
+    }, 1000);
+})
+promiseFour.then((user)=>{
     console.log(user);
-});
+    return user.username
+}).then((username)=>{
+    console.log(username);
+}).catch(function(error){
+    console.log(error);
+})
