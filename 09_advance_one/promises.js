5183176ff1b42d0b0e5dd9c8c4470ccd8429aca8
Mohit <ai.workspace.mohit@gmail.com>
2026-09-02 16:37:13 +0000

    fetch syntax

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -34,21 +34,79 @@
 //     console.log(user);
 // });
 
-const promiseFour = new Promise(function(resolve, reject){
+// const promiseFour = new Promise(function(resolve, reject){
+//     setTimeout(function(){
+//         let error = false;
+//         if(!error){
+//             resolve({username:"mohit", email:"mohit@example.com"});
+//         } else {
+//             reject('Error: Something went wrong');
+//         }
+//     }, 1000);
+// })
+// promiseFour.then((user)=>{
+//     console.log(user);
+//     return user.username
+// })
+// .then((username)=>{
+//     console.log(username);
+// })
+// .catch(function(error){
+//     console.log(error);
+// }).finally(()=> {
+//     console.log("The promise is either resolved or rejected");
+// })
+
+const promiseFive = new Promise(function(resolve, reject){
     setTimeout(function(){
-        let error = true;
+        let error = false;
         if(!error){
-            resolve({username:"mohit", email:"mohit@example.com"});
+            resolve({username: "javascript", password: "123"})
         } else {
-            reject('Error: Something went wrong');
+            reject('Error: JS Something went wrong')
         }
-    }, 1000);
+    }, 1000)
+});
+
+async function consumePromiseFive(){
+    try{
+        const response = await promiseFive
+        console.log(response);
+    }
+    catch (error){
+        console.log(error);
+    }
+    
+}
+
+consumePromiseFive();
+
+
+
+// async function getAllUsers(){
+//     try{
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users')
+//         // console.log(response);
+
+//         const data = await response.json()
+//         console.log(data);
+
+//     } catch (error){
+//         console.log("E: ", error);
+//     }
+
+// }
+
+// getAllUsers()
+
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then((response) => {
+    return response.json()
 })
-promiseFour.then((user)=>{
-    console.log(user);
-    return user.username
-}).then((username)=>{
-    console.log(username);
-}).catch(function(error){
-    console.log(error);
+.then((data) => {
+    console.log(data);
+})
+.catch((error) => {
+    console.log("E: ", error);
 })
